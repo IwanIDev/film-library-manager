f1c603814e75aa7ecae38be78d9215c1cb3713de
IwanIDev <iwan@iwani.dev>
2026-06-30 14:23:48 +0100

    Configured clangd language server

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -4,11 +4,23 @@
 #include <QMainWindow>
 #include <string>
 
+/*
+* The MainWindow class represents the main window of the Film Library Manager application.
+*/
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
     public:
+        /*
+         * Constructor for the MainWindow class.
+         * @param parent The parent widget (default is nullptr).
+         * @param title The title of the window (default is "Film Library Manager").
+         */
         MainWindow(QWidget *parent = nullptr, const std::string &title = "Film Library Manager");
+
+        /*
+         * Destructor for the MainWindow class.
+         */
         ~MainWindow();
 };
 
